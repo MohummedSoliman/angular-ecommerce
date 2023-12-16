e1a808f285e8afb0df51939e7bcd03f28c09ec5e
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-12-16 08:29:11 +0200

    create new commponent for desplay prodcut details

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,12 +1,40 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { Product } from '../../model/product';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, CommonModule, RouterModule],
+  providers: [ProductService],
   templateUrl: './product-details.component.html',
-  styleUrl: './product-details.component.css'
+  styleUrl: './product-details.component.css',
 })
 export class ProductDetailsComponent {
+  product!: Product;
 
+  constructor(
+    public productService: ProductService,
+    public route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(() => {
+      this.handleProductDetails();
+    });
+  }
+
+  handleProductDetails() {
+    const hasProductId = this.route.snapshot.paramMap.has('id');
+
+    if (hasProductId) {
+      const productId: number = +this.route.snapshot.paramMap.get('id')!;
+      this.productService
+        .getProductDetails(productId)
+        .subscribe((data) => (this.product = data));
+    }
+  }
 }
