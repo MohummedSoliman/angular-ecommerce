c52bc8f5fb8ad849ff3cea893627f65ce5a6ecc5
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-12-16 16:47:28 +0200

    handle methods and refactor the code

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -33,6 +33,8 @@ export class ProductListComponent {
   thePageSize: number = 5;
   theTotalElements: number = 0;
 
+  previousKeyword: string = '';
+
   constructor(
     private productService: ProductService,
     private route: ActivatedRoute
@@ -57,9 +59,19 @@ export class ProductListComponent {
   handleSearchProduct() {
     const searchValue: string = this.route.snapshot.paramMap.get('keyword')!;
 
+    if (this.previousKeyword != searchValue) {
+      this.thePageNumber = 1;
+    }
+
+    this.previousKeyword = searchValue;
+
     this.productService
-      .getProductByKeyword(searchValue)
-      .subscribe((data) => (this.products = data));
+      .searchProductPaginate(
+        this.thePageNumber - 1,
+        this.thePageSize,
+        searchValue
+      )
+      .subscribe(this.processResult());
   }
 
   handleListProduct() {
@@ -85,17 +97,21 @@ export class ProductListComponent {
         this.thePageSize,
         this.currentCategoryId
       )
-      .subscribe((data) => {
-        this.products = data._embedded.products;
-        this.thePageNumber = data.page.number + 1;
-        this.thePageSize = data.page.size;
-        this.theTotalElements = data.page.totalElements;
-      });
+      .subscribe(this.processResult());
   }
 
   updatePageSize(pageSize: string) {
     this.thePageSize = +pageSize;
     this.thePageNumber = 1;
     this.listProducts();
+  }
+
+  processResult() {
+    return (data: any) => {
+      this.products = data._embedded.products;
+      this.thePageNumber = data.page.number + 1;
+      this.thePageSize = data.page.size;
+      this.theTotalElements = data.page.totalElements;
+    };
   }
 }
