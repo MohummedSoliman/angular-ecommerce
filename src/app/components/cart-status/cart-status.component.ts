9ff363c3529ea174e04dc6d23bcf18dc56672cd5
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-12-17 09:07:16 +0200

    inject cart service and subscribe

--- src/app/components/cart-status/cart-status.component.ts
+++ src/app/components/cart-status/cart-status.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-cart-status',
   standalone: true,
-  imports: [],
+  imports: [CartService],
   templateUrl: './cart-status.component.html',
-  styleUrl: './cart-status.component.css'
+  styleUrl: './cart-status.component.css',
 })
 export class CartStatusComponent {
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
 
+  constructor(public cartServcie: CartService) {}
+
+  ngOnInit() {
+    this.updateCartStatus();
+  }
+
+  updateCartStatus() {
+    this.cartServcie.totalPrice.subscribe((data) => (this.totalPrice = data));
+
+    this.cartServcie.totalQuantity.subscribe(
+      (data) => (this.totalQuantity = data)
+    );
+  }
 }
