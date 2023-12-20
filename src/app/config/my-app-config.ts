4e5e0f4c681e8af86190858f5e4d2a7c3d12833b
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-12-20 13:51:30 +0200

    add configuration about okta authentication server

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+  oidc: {
+    clientId: '0oae0wkd7a8lBukGe5d7',
+    issuer: 'https://dev-03161670.okta.com/oauth2/default',
+    redirectUri: 'http://172.17.0.2:4200/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+  },
+};
