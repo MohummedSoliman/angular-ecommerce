67796dcabc085a437b17e0a2a1f7efbe2f7b8687
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-12-20 15:42:09 +0200

    add login status component to track user status

---
+++ src/app/components/login-status/login-status.component.ts
@@ -0,0 +1,39 @@
+import { Component, Inject } from '@angular/core';
+import { OktaAuthStateService, OKTA_AUTH } from '@okta/okta-angular';
+import OktaAuth from '@okta/okta-auth-js';
+
+@Component({
+  selector: 'app-login-status',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-status.component.html',
+  styleUrl: './login-status.component.css',
+})
+export class LoginStatusComponent {
+  isAuthenticated: boolean = false;
+  userFullName: string = '';
+
+  constructor(
+    private oktaAuthService: OktaAuthStateService,
+    @Inject(OKTA_AUTH) private oktaAuth: OktaAuth
+  ) {}
+
+  ngOnInit() {
+    this.oktaAuthService.authState$.subscribe((result) => {
+      this.isAuthenticated = result.isAuthenticated!;
+      this.getUserDetails();
+    });
+  }
+
+  getUserDetails() {
+    if (this.isAuthenticated) {
+      this.oktaAuth.getUser().then((res) => {
+        this.userFullName = res.name as string;
+      });
+    }
+  }
+
+  logout() {
+    this.oktaAuth.signOut();
+  }
+}
